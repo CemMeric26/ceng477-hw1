e8d53392ec1285c4befa91d4de7eb39830649773
CemMeric26 <meric.sefikogullari@metu.edu.tr>
2023-10-28 16:50:36 +0300

    start base

--- code_template/raytracer.cpp
+++ code_template/raytracer.cpp
@@ -4,6 +4,54 @@
 
 typedef unsigned char RGB[3];
 
+
+RGB background_color;
+
+void Main_raytrace_Computer(parser::Scene scene)
+{
+    parser::Vec3i background_color = scene.background_color;
+    float shadow_ray_epsilon = scene.shadow_ray_epsilon;
+    int max_recursion_depth = scene.max_recursion_depth;
+    std::vector<parser::Camera> cameras = scene.cameras;
+    parser::Vec3f ambient_light = scene.ambient_light;
+    std::vector<parser::PointLight> point_lights = scene.point_lights;
+    std::vector<parser::Material> materials = scene.materials;
+    std::vector<parser::Vec3f> vertex_data = scene.vertex_data;
+    std::vector<parser::Mesh> meshes = scene.meshes;
+    std::vector<parser::Triangle> triangles = scene.triangles;
+    std::vector<parser::Sphere> spheres = scene.spheres;
+
+    // for each camera
+    for(int i=0; i<cameras.size(); i++)
+    {
+        // to calculate the image we need followings:
+        // camera (cam_position, cam_gaze, up, nearplane, near_dist, image_w, image_h, image_name)
+        // lights (ambient, point_lights)
+        // materials (ambient, diffuse, specular, mirror, phong_exponent)
+        // vertex_data (vertex)
+        // meshes (material_id, faces)
+        // triangles (material_id, indices)
+        // spheres (material_id, center_vertex_id, radius)
+        //  compute_color() and apply_Shading() functions
+
+
+        parser::Camera camera = cameras[i];
+        parser::Vec3f cam_position = camera.position;
+        parser::Vec3f cam_gaze = camera.gaze;
+
+
+
+
+    }
+
+    
+}
+
+void Child_raytrace_Computer()
+{
+    ;
+}
+
 int main(int argc, char* argv[])
 {
     // Sample usage for reading an XML scene file
@@ -47,6 +95,7 @@ int main(int argc, char* argv[])
         }
     }
 
+    //  You should render as many images as the number of cameras.
     write_ppm("test.ppm", image, width, height);
 
 }
